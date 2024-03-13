b4582688713774ad8c8baded4d9e6b71bfb0b8ba
Victoria <93460254+vickitine404@users.noreply.github.com>
2024-03-13 16:48:40 -0700

    printing shooter

--- src/main/java/frc/robot/autos/shoot.java
+++ src/main/java/frc/robot/autos/shoot.java
@@ -14,14 +14,21 @@ public class shoot extends Command{
         this.Launcher = s_Launcher;
     }
 
+    private boolean run = true;
+    @Override
+    public void initialize() {
+      System.out.println("ClimberCommand started!");
+    }
+
     @Override
     public void execute() {
-        
+        if (!run) return;
+        run = false;
 
         try {
-
-            Indexer.setMotors(8);
             Launcher.setMotors(6);
+            Indexer.setMotors(8);
+            
             Thread.sleep(1000);
 
         } catch (Exception e){
@@ -33,5 +40,12 @@ public class shoot extends Command{
 
     @Override
     public void end(boolean interrupted) {
+        Launcher.setMotors(0);
+        Indexer.setMotors(0);
+        System.out.println("finsh");
+    }
+    @Override
+    public boolean isFinished() {
+      return false;
     }
 }
